f6a7b6625f8a0beb0393e956c8410f66ae8b4d8c
Baratin.pro <baratin.pro@gmail.com>
2022-04-02 12:41:19 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+
+app.use(helmet());
+
+mongoose
+    .connect(process.env.MONGO_LINK, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connection to MongoDB successfull !'))
+    .catch(() => console.log('Connexion to MongoDB fail !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
+    next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID invalid';
+        } else {
+            req.user = decodedToken;
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Request invalid !'),
+        });
+    }
+};
