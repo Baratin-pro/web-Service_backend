494c7fe0fcc41c00ae0d092d8e57cba35fe62252
Baratin.pro <baratin.pro@gmail.com>
2022-05-12 19:00:00 +0200

    Add account function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,9 +19,9 @@ exports.signup = async (req, res) => {
       return res.status(400).json({ message: error.details[0].message });
     }
 
-    const emailDb = await db.user.findOne({ email: req.body.email });
-    const username = await db.user.findOne({ username: req.body.username });
-    if (emailDb || username) {
+    const emailDb = await getByEmail(req.body.email);
+    const usernameDb = await db.user.findOne({ username: req.body.username });
+    if (emailDb || usernameDb) {
       return res.status(400).json({ message: 'Email or username already used' });
     }
 
@@ -43,7 +43,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: 'Incorrect entry' });
     }
 
-    const userDb = await db.user.findOne({ email: req.body.email });
+    const userDb = await getByEmail(req.body.email);
     if (!userDb) {
       return res.status(404).json({ message: 'Email or password invalid' });
     }
@@ -52,7 +52,7 @@ exports.login = async (req, res) => {
     if (!passwordValid) {
       return res.status(404).json({ message: 'Email or password invalid' });
     }
-    res.status(200).json({
+    return res.status(200).json({
       token: jwt.sign({ userId: userDb._id }, process.env.TOKEN_SECRET, {
         expiresIn: '24h',
       }),
@@ -61,3 +61,24 @@ exports.login = async (req, res) => {
     return res.status(500).json(e);
   }
 };
+
+exports.account = async (req, res) => {
+  try {
+    let userDb = await getById(req.user.userId);
+    if (!userDb) {
+      return res.status(401).json({ message: 'Request invalid !' });
+    }
+    userDb.password = undefined;
+    return res.status(200).json(userDb);
+  } catch (e) {
+    return res.status(500).json(e);
+  }
+};
+
+async function getById(id) {
+  return await db.user.findOne({ _id: id });
+}
+
+async function getByEmail(email) {
+  return await db.user.findOne({ email: email });
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,10 +4,12 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.controller');
 const rateLimite = require('../middleware/rateLimit');
+const auth = require('../middleware/auth');
 
 module.exports = (app) => {
   router.post('/signup', userCtrl.signup);
   router.post('/login', rateLimite, userCtrl.login);
+  router.get('/account', auth, userCtrl.account);
 
   app.use('/api/user', router);
 };
